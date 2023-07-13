5b58f5602d8f5d68d6c37e57ff1a7ebf7d2824d9
MGFSebanes <138366282+MGFSebanes@users.noreply.github.com>
2023-07-13 10:55:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,47 +48,7 @@ import { Route, Routes, useNavigate, HashRouter} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Routes>
-              <Route path="/" element={<SmartTransaction />}></Route>
-              <Route path="/Profile" element={<Profile />}>
-                <Route path="/Profile/StudentProfile" element={<Studentprofile />}></Route>
-                <Route path="/Profile/Offices" element={<div>Offices</div>}></Route>
-                <Route path="/Profile/Documents" element={<div>Documents</div>}></Route>
-                <Route path="/Profile/Request" element={<Student />}></Route>
-                <Route path="/Profile/Appointment" element={<Appointment />}></Route>
-                <Route path="/Profile/RequestAppointment" element={<RequestAppointment />}></Route>
-
-              </Route>
-              <Route path="/Newprofile" element={<Newprofile/>}></Route>
-              <Route path="/AcadProfile" element={<AcadProfile/>}>
-                <Route path="/AcadProfile/FacultyProfile" element={<FacultyProfile/>}></Route>
-                <Route path="/AcadProfile/Message" element={<div>Message</div>}></Route>
-              <Route path="/AcadProfile/Documents" element={<div>Documents</div>}></Route>
-              </Route>
-              <Route path="/Admin" element={<Admin/>}>
-              <Route path="/Admin/Blogpost" element={<BlogSpot/>}></Route>
-              <Route path="/Admin/Dashboard" element={<Dashboard/>}></Route>
-              <Route path="/Admin/Message" element={<div>Message</div>}></Route>
-              <Route path="/Admin/Users" element={<Usertable/>}></Route>
-              <Route path="/Admin/Transactions" element={<Transactions/>}></Route>
-              <Route path="/Admin/Userlogs" element={<UserLog/>}></Route>
-              </Route>
-              <Route path="/Newvisitor" element={<NewprofileVisitor/>}></Route>
-              <Route path="/Visitor" element={<VisitorProfile/>}>
-              <Route path="/Visitor/visitorprofile" element={<VisitorsProfile/>}></Route>
-              <Route path="/Visitor/Offices" element={<div>Offices</div>}></Route>
-                <Route path="/Visitor/Documents" element={<div>Documents</div>}></Route>
-                <Route path="/Visitor/Request" element={<Student />}></Route>
-                <Route path="/Visitor/Appointment" element={<Appointment />}></Route>
-                <Route path="/Visitor/RequestAppointment" element={<RequestAppointment />}></Route>
-              </Route>
-              
-               
-              
-           </Routes>
-           
-      </HashRouter>
+    <Dashboard />
     </div>
   );
 }
